353faad362012f52ac8939b0d404d408189a899a
mabertha.ab <thataaberlian@gmail.com>
2026-01-27 11:25:09 +0700

    add Models Category and Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        "name",
+        "description",
+        "quantity",
+        "category_id",
+        "price",
+        "status",
+        "image",
+        
+
+    ];
+
+    public function category()  {
+        return $this->belongsTo(Category::class);
+        
+    }
 }
