1210a2591cba96a5d6fccc60566c5a1ed4d087c5
mabertha.ab <thataaberlian@gmail.com>
2026-01-28 22:48:31 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products',[ProductController::class,'index'])->name('product.index');
+Route::get('/create-product',[ProductController::class,'create'])->name('product.create');
