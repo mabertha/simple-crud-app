e6d1d2d0bb839eff14d07ff346391f074a6e8491
mabertha.ab <thataaberlian@gmail.com>
2026-02-09 21:38:15 +0700

    Update Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,12 +19,29 @@ class ProductController extends Controller
         $products = $query->latest()->paginate(8);
 
         return view("product.product-list",compact("products"));
-    }
+    } 
 
     public function create(){
         $categories = Category::all();
         return view("product.create",compact("categories"));
     }
 
-   
+    public function store(Request $request){
+        $validated = $request->validate([
+            "name"=> "required|string",
+            "description"=> "nullable|string",
+            "price"=> "required|numeric",
+            "quantity"=> "required|numeric",
+            "status"=> "required",
+            "category_id"=> "required",
+            "image"=> "nullable|image|mimes:jpg,png",
+
+            ]);
+            if ($request->hasFile("image")) {
+                $validated["image"] = $request->file("image")->store("products","public");
+            }
+            Product::create($validated);
+
+            return redirect()->route("product.index")->with("success","product added successfully");
+        }
 }
